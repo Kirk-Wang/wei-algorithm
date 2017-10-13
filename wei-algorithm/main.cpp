cc9580f4a60cb4ba0c72196c307865e1e836a8cc
zuowei <wangzuowei@yy.com>
2017-10-14 02:54:14 +0800

    Initial Commit

---
+++ wei-algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wei-algorithm
+//
+//  Created by ling on 17/10/14.
+//  Copyright © 2017年 ling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
